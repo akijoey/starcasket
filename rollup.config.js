f23913e5f77194ed60cc0769c08bd7fb926d7396
akijoey <akijoey@akijoey.com>
2022-09-10 20:37:52 +0800

    fix: export declaration files

--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,7 @@ const { nodeResolve } = require('@rollup/plugin-node-resolve')
 const commonjs = require('@rollup/plugin-commonjs')
 const { babel } = require('@rollup/plugin-babel')
 const { minify } = require('rollup-plugin-esbuild')
+const { default: dts } = require('rollup-plugin-dts')
 
 const project = require('./package.json')
 const year = new Date().getFullYear()
@@ -12,29 +13,40 @@ const env = process.env.NODE_ENV
 const isEnvDevelopment = env === 'development'
 const isEnvProduction = env === 'production'
 
+const input = 'src/index.ts'
 const extensions = ['.js', '.ts', '.json']
+const banner = `/*! @license ${project.license} (c) ${year} ${project.author} */`
 
-module.exports = {
-  input: 'src/index.ts',
-  output: {
-    file: project.main,
-    format: 'umd',
-    name: project.name,
-    banner:
-      isEnvProduction &&
-      `/*! @license ${project.license} (c) ${year} ${project.author} */`,
-    sourcemap: isEnvDevelopment
+module.exports = [
+  {
+    input,
+    output: {
+      file: project.main,
+      format: 'umd',
+      name: project.name,
+      banner: isEnvProduction && banner,
+      sourcemap: isEnvDevelopment
+    },
+    plugins: [
+      nodeResolve({
+        extensions
+      }),
+      commonjs(),
+      babel({
+        babelHelpers: 'runtime',
+        exclude: /node_modules/,
+        extensions
+      }),
+      isEnvProduction && minify()
+    ].filter(Boolean)
   },
-  plugins: [
-    nodeResolve({
-      extensions
-    }),
-    commonjs(),
-    babel({
-      babelHelpers: 'runtime',
-      exclude: /node_modules/,
-      extensions
-    }),
-    isEnvProduction && minify()
-  ].filter(Boolean)
-}
+  isEnvProduction && {
+    input,
+    output: {
+      file: project.types,
+      format: 'esm',
+      banner
+    },
+    plugins: [dts()]
+  }
+].filter(Boolean)
