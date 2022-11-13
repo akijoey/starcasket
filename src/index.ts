d57083971dd18aecd8bf633a647346e1dd880893
akijoey <akijoey@akijoey.com>
2022-11-13 18:58:16 +0800

    fix: export image

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as arrayExports from './extends/array'
 import * as functionExports from './extends/function'
+import * as imageExports from './extends/image'
 import * as objectExports from './extends/object'
 import * as timerExports from './encaps/timer'
 import * as requestExports from './encaps/request'
@@ -7,6 +8,7 @@ import * as requestExports from './encaps/request'
 const stars = [
   arrayExports,
   functionExports,
+  imageExports,
   objectExports,
   timerExports,
   requestExports
