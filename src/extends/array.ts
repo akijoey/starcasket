84c2b4ca7427f9b31ba3345f9fd5dfc184ec273a
akijoey <akijoey@akijoey.com>
2022-11-17 15:04:47 +0800

    fix: array partition

--- src/extends/array.ts
+++ src/extends/array.ts
@@ -14,12 +14,28 @@ const unique = function unique(this: any[]): any {
   return [...new Set(this)]
 }
 
+const partition = function partition(
+  this: any[],
+  callback: (item: any, index: number, array: any[]) => any,
+  context?: any
+): any[] {
+  return this.reduce(
+    (res, item, index, array) => {
+      const cond: boolean = callback.call(context, item, index, array)
+      res[cond ? 0 : 1].push(item)
+      return res
+    },
+    [[], []]
+  )
+}
+
 const install = (): void => {
   Object.assign(Array.prototype, {
     swap,
     shuffle,
-    unique
+    unique,
+    partition
   })
 }
 
-export { swap, shuffle, unique, install }
+export { swap, shuffle, unique, partition, install }
