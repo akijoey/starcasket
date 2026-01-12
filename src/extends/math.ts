29ada252917cc25d00aef7a1a359281d5b0c5906
akijoey <akijoey@akijoey.com>
2026-01-12 11:37:40 +0000

    fix: precise math method

--- src/extends/math.ts
+++ src/extends/math.ts
@@ -6,9 +6,24 @@ export const fract = function fract(num: number): number {
   return Number((num % 1).toFixed(digits(num)))
 }
 
+export const roundTo = function roundTo(num: number, precision = 0) {
+  return Math.round(Number(num + 'e' + precision)) / Math.pow(10, precision)
+}
+
+export const floorTo = function floorTo(num: number, precision = 0) {
+  return Math.floor(Number(num + 'e' + precision)) / Math.pow(10, precision)
+}
+
+export const ceilTo = function ceilTo(num: number, precision = 0) {
+  return Math.ceil(Number(num + 'e' + precision)) / Math.pow(10, precision)
+}
+
 export const install = (): void => {
   Object.assign(Math, {
     digits,
-    fract
+    fract,
+    roundTo,
+    floorTo,
+    ceilTo
   })
 }
