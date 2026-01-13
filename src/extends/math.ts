f10f5887d9a87eeb8e2573bdd95588697bd08453
akijoey <akijoey@akijoey.com>
2026-01-13 10:26:48 +0000

    fix: basic math method

--- src/extends/math.ts
+++ src/extends/math.ts
@@ -1,9 +1,25 @@
-export const digits = function digits(num: number): number {
-  return num.toString().split('.')[1]?.length ?? 0
+export const add = function add(augend: number, addend: number) {
+  return augend + addend
 }
 
-export const fract = function fract(num: number): number {
-  return Number((num % 1).toFixed(digits(num)))
+export const subtract = function subtract(minuend: number, subtrahend: number) {
+  return minuend - subtrahend
+}
+
+export const multiply = function multiply(multiplier: number, multiplicand: number) {
+  return multiplier * multiplicand
+}
+
+export const divide = function divide(dividend: number, divisor: number) {
+  return dividend / divisor
+}
+
+export const sum = function sum(...args: number[]) {
+  return args.reduce((sum, num) => sum + num, 0)
+}
+
+export const mean = function mean(...args: number[]) {
+  return sum(...args) / args.length
 }
 
 export const roundTo = function roundTo(num: number, precision = 0) {
@@ -18,12 +34,26 @@ export const ceilTo = function ceilTo(num: number, precision = 0) {
   return Math.ceil(Number(num + 'e' + precision)) / Math.pow(10, precision)
 }
 
+export const digits = function digits(num: number): number {
+  return num.toString().split('.')[1]?.length ?? 0
+}
+
+export const fract = function fract(num: number): number {
+  return Number((num % 1).toFixed(digits(num)))
+}
+
 export const install = (): void => {
   Object.assign(Math, {
-    digits,
-    fract,
+    add,
+    subtract,
+    multiply,
+    divide,
+    sum,
+    mean,
     roundTo,
     floorTo,
-    ceilTo
+    ceilTo,
+    digits,
+    fract
   })
 }
